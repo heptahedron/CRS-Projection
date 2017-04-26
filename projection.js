978d7519725b0ff14d56dff62c1ef32977658d03
Braxton </home/braxton/Projects/CRS-Projection/~@braxton.codes>
2017-04-26 14:11:27 -0500

    Remove implicit dependency on `data.json` file
    If another developer wanted to use this package to convert their own
    GeoJSON data, they would have to move the file into the package folder
    and rename it to `data.json`, then they would have to start the
    `webpack-dev-server`, open a browser, and copy-and-paste the value
    displayed there. By removing the import that uses `data.json` and
    instead making a function to which this data will be passed (in the
    form of the parameter `featureCollection`), this code becomes far more
    versatile and easier to use. Now, there doesn't even have to be a
    file--if they generated some coordinates somehow, they could simply
    pass them to our function instead of saving that output to a file and
    going through the other process. If they wanted these new coordinates
    saved to disk, they would have the option of doing so in whatever
    manner worked for them, be it printing them to the console and copying
    them or using Node's filesystem functions to send it directly to a file.e

--- projection.js
+++ projection.js
@@ -1,4 +1,3 @@
-import data from './data.json';
 import proj4 from 'proj4';
 
 const esriProjection = '+proj=tmerc +lat_0=31 +lon_0=-106.25 +k=0.9999 +x_0=500000.0000000001 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=us-ft +no_defs';
@@ -8,22 +7,24 @@ function changeCoords(coordinates){
 	return proj4(esriProjection, wgs84Projection, coordinates);
 }
 
-for (var i = 0; i < data.features.length; i++) {
-  const projCoord =
-        changeCoords(data.features[i].geometry.coordinates[0],
-                     data.features[i].geometry.coordinates[1])
+function esriToWsgi84(featureCollection) {
+  for (var i = 0; i < featureCollection.features.length; i++) {
+    const projCoord =
+          changeCoords(featureCollection.features[i].geometry.coordinates[0],
+                       featureCollection.features[i].geometry.coordinates[1])
 
-	data.features[i].geometry.coordinates[0] = projCoord[0];
-	data.features[i].geometry.coordinates[1] = projCoord[1];
-}
+	  featureCollection.features[i].geometry.coordinates[0] = projCoord[0];
+	  featureCollection.features[i].geometry.coordinates[1] = projCoord[1];
+  }
 
-var dataFramework = {
-  "type": "FeatureCollection",
-  "crs": {
-    "type": "name",
-    "properties": {
-      "name": "urn:ogc:def:crs:OGC:1.3:CRS84"
-    }
-  },
-  "features": data.features
+  return {
+    "type": "FeatureCollection",
+      "crs": {
+        "type": "name",
+        "properties": {
+          "name": "urn:ogc:def:crs:OGC:1.3:CRS84"
+        }
+      },
+      "features": featureCollection.features
+  }
 }
