160922ee6a00c5ff373f85483efe544272b7a65f
Braxton </home/braxton/Projects/CRS-Projection/~@braxton.codes>
2017-04-26 14:05:58 -0500

    Move `projCoord` variable closer to where it's used
    Since `projCoord` is only being used within this loop, it can be
    confusing to see it declared far away from there. There's also no
    reason for it to be set to 0 initially; it's a coordinate, not a
    number, and we would never want to accidentally try to use it like a
    coordinate when it was a number.

--- projection.js
+++ projection.js
@@ -4,15 +4,14 @@ import proj4 from 'proj4';
 const esriProjection = '+proj=tmerc +lat_0=31 +lon_0=-106.25 +k=0.9999 +x_0=500000.0000000001 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=us-ft +no_defs';
 const wgs84Projection = "+proj=longlat +datum=WGS84 +no_defs";
 
-let projCoord = 0;
-
 function changeCoords(coordinates){
 	return proj4(esriProjection, wgs84Projection, coordinates);
 }
 
 for (var i = 0; i < data.features.length; i++) {
-
-	projCoord = changeCoords(data.features[i].geometry.coordinates[0], data.features[i].geometry.coordinates[1])
+  const projCoord =
+        changeCoords(data.features[i].geometry.coordinates[0],
+                     data.features[i].geometry.coordinates[1])
 
 	data.features[i].geometry.coordinates[0] = projCoord[0];
 	data.features[i].geometry.coordinates[1] = projCoord[1];
