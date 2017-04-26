05ed232ca68418796b81276d5a808dd577c4057a
Braxton </home/braxton/Projects/CRS-Projection/~@braxton.codes>
2017-04-26 13:38:07 -0500

    Rename projection variables
    It looks like you got these variable names from example code that
    proj4js has in their documentation. Those names are useful for them to
    show how the order of the arguments affects the output of their
    projection function, but they do little to help developers reading our
    code understand why or what it is that we're doing.

--- projection.js
+++ projection.js
@@ -1,16 +1,14 @@
 import data from './data.json';
 import proj4 from 'proj4';
 
-// ORIGINAL DATA PROJECTION i.e what you are projecting from
-let firstProjection = '+proj=tmerc +lat_0=31 +lon_0=-106.25 +k=0.9999 +x_0=500000.0000000001 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=us-ft +no_defs';
-// WGS 84 EPSG: 4326 i.e what you are projecting to
-let secondProjection = "+proj=longlat +datum=WGS84 +no_defs";
+let esriProjection = '+proj=tmerc +lat_0=31 +lon_0=-106.25 +k=0.9999 +x_0=500000.0000000001 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=us-ft +no_defs';
+let wgs84Projection = "+proj=longlat +datum=WGS84 +no_defs";
 
 let projCoord = 0;
 
 function changeCoords(coordinates){
 
-	return proj4(firstProjection , secondProjection, [data.features[i].geometry.coordinates[0], data.features[i].geometry.coordinates[1]]);
+	return proj4(esriProjection , wgs84Projection, [data.features[i].geometry.coordinates[0], data.features[i].geometry.coordinates[1]]);
 }
 
 for (var i = 0; i < data.features.length; i++) {
